0924d1fe7e8099ab4773a5846607c8a23028ae03
Muhammad Tausif <tausifasia@gmail.com>
2024-02-15 21:43:00 +0500

    Python String.count() method.

---
+++ count.py
@@ -0,0 +1,19 @@
+
+
+feedback = """
+                The delivery was fast, and the packaging was excellent. 
+                Excellent service overall. Will recommend!
+            """
+keyword = "excellent"
+            
+print(f"The word '{keyword}' appears {feedback.count(keyword)} times in the feedback.")
+
+
+text = """
+        Fear leads to anger; anger leads to hate;
+        hate leads to suffering.
+        This is a path to the dark side.
+       """
+word = "leads"
+
+print(f"The word '{word}' appears {text.count(word)} times.")
